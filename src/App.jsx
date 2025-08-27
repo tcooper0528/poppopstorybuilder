1be93e8425565638e5e9c59c451934b0d7864a9e
tcooper0528 <tcooper0528@gmail.com>
2025-08-27 10:45:55 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StorybookBuilder from './components/StorybookBuilder_PagesPromptsPDFUploads.jsx'
+export default function App(){
+  return (
+    <div style={{padding:24}}>
+      <h1>PopPop Builder</h1>
+      <StorybookBuilder />
+    </div>
+  )
+}
---
+++ src/components/StorybookBuilder_PagesPromptsPDFUploads.jsx
@@ -0,0 +1,3 @@
+import React, { useState } from 'react'
+import { jsPDF } from 'jspdf'
+export default function StorybookBuilder_PagesPromptsPDFUploads(){ return <div>Builder Ready</div> }
