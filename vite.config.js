32610b8a731c998c0c00b392ac2c2b8233e2a048
tcooper0528 <tcooper0528@gmail.com>
2025-08-29 13:35:40 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+});
