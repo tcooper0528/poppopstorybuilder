f09aea243ea38a3756654a6e039781ac33872568
tcooper0528 <tcooper0528@gmail.com>
2025-08-29 14:28:53 -0500

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import UnifiedStorybookBuilder from "./UnifiedStorybookBuilder";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-background text-foreground">
-      <UnifiedStorybookBuilder />
-    </div>
-  );
-}
